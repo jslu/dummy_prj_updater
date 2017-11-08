34ef7af79da1ef2b6c0e6e43b2f42f9ac3898417
Joe.Lu <joe.lu@aylanetworks.com>
2017-11-09 04:52:25 +0800

    Update the code and install jq

--- checkout_latest_source.sh
+++ checkout_latest_source.sh
@@ -4,6 +4,10 @@ cat /var/run/secrets/boostport.com/vault-token | jq -r .clientToken > /tmp/token
 
 curl -X GET -H"X-VAULT-TOKEN: `cat /var/run/secrets/boostport.com/vault-token | jq -r .clientToken`" "http://tc-staging.ayla.com.cn:8200/v1/secret/ecryptfs-passphrase" > /tmp/passphrase.txt
 
+# PASSPHRASE="<passphrase>"
+# echo -n $PASSPHRASE | ecryptfs-add-passphrase		# get the sig into $SIG
+# mount -t ecryptfs -o key=passphrase:passphrase_passwd=$PASSPHRASE,no_sig_cache=yes,verbose=no,ecryptfs_sig=$SIG,ecryptfs_cipher=aes,ecryptfs_key_bytes=16,ecryptfs_passthrough=no,ecryptfs_enable_filename_crypto=no /ayla/dummy_prj
+
 mkdir -p /ayla/dummy_prj
 
 cd /ayla/dummy_prj
