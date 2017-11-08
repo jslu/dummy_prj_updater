0dd4968fdd7c3f5d59c72e9d73a93733f1e3f4a0
Ayla-Joe.Lu <joe.lu@aylanetworks.com>
2017-11-09 06:05:04 +0800

    Fix the bug of non-existent folder 'releases'

--- checkout_latest_source.sh
+++ checkout_latest_source.sh
@@ -4,7 +4,7 @@ cat /var/run/secrets/boostport.com/vault-token | jq -r .clientToken > /tmp/token
 
 curl -X GET -H"X-VAULT-TOKEN: `cat /var/run/secrets/boostport.com/vault-token | jq -r .clientToken`" "http://tc-staging.ayla.com.cn:8200/v1/secret/ecryptfs-passphrase" > /tmp/passphrase.txt
 
-mkdir -p /ayla/dummy_prj
+mkdir -p /ayla/dummy_prj/releases
 
 cd /ayla/dummy_prj
 FOLDER="releases/`date +'%Y-%m-%d-%s'`"
